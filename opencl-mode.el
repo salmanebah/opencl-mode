ec5fb85eceae048912f79f5c6444ddb52d1b4d64
Salmane Bah <truegforreal@gmail.com>
2014-09-14 02:23:45 +0200

    fixed #1 and installation guidelines added in opencl-mode.el

--- opencl-mode.el
+++ opencl-mode.el
@@ -20,13 +20,15 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;; Installation:
+;; 1- Put this file in your load path or optionally add this line to your Emacs init file:
+;;    (add-to-list 'load-path "/path/to/directory/where/opencl-mode.el/resides")
+;; 2- Add these lines to your Emacs init file
+;;    (require 'opencl-mode)
+;;    (add-to-list 'auto-mode-alist '("\\.cl\\'" . opencl-mode))
+;; 3- Enjoy :)
+
 ;;; Code:
-
-;;;###autoload  regexp string for opencl symbol class
-;;(eval-when-compile
-  ;;(require 'c-mode))
-
-
 
 
 (defvar-local *opencl-extension-color* "#A82848"
@@ -37,31 +39,35 @@
   "custom face for cl-extension"
   :group 'opencl-faces)
 
-;;(defvar-local font-lock-opencl-face 'font-lock-opencl-face)
+(defmacro define-local-var (variable value &optional docstring)
+  "Define `VARIABLE' with `VALUE' as local to this buffer with `DOCSTRING'."
+  `(if (= emacs-major-version 24)
+       (defvar-local ,variable ,value ,docstring)
+     (make-local-variable (defvar ,variable ,value ,docstring))))
 
-(defvar-local opencl-keywords-regexp
+(define-local-var opencl-keywords-regexp
   "\\(__\\)?\\(kernel\\|global\\|local\\|constant\\|private\\|read_only\\|write_only\\|read_write\\|enable\\|disable\\)[[:blank:]\n]+")
 
-(defvar-local opencl-functions-regexp (regexp-opt '("get_work_dim" "get_global_size"
+(define-local-var opencl-functions-regexp (regexp-opt '("get_work_dim" "get_global_size"
  						    "get_local_size" "get_global_id"
 						    "get_local_id" "get_num_groups"
 						    "get_group_id" "get_global_offset") 'words))
 
-(defvar-local opencl-constant-regexp (regexp-opt '("MAXFLOAT" "HUGE_VALF"
+(define-local-var opencl-constant-regexp (regexp-opt '("MAXFLOAT" "HUGE_VALF"
 						   "INFINITY" "NAN" "HUGE_VAL") 'words))
 ;; TODO : image datatype , ptr data
-(defvar-local opencl-types-regexp
+(define-local-var opencl-types-regexp
   "\\(char\\|short\\|half\\|int\\|double\\|float\\|long\\|uchar\\|ushort\\|uint\\|ulong\\)[[:digit:]]\\{0,2\\}[[:blank:]\n]+")
 
-(defvar-local opencl-image-type-regexp (regexp-opt '("image2d_t" "image3d_t"
+(define-local-var opencl-image-type-regexp (regexp-opt '("image2d_t" "image3d_t"
 						     "image2d_array_t" "image3d_array_t"
 						     "image1d_array_t" "image1d_t"
 						     "image1d_buffer_t" "sampler_t"
 						     "event_t") 'words))
 
-(defvar-local opencl-extension-regexp "cl_khr_[a-zA-Z][a-zA-Z_0-9]+")
+(define-local-var opencl-extension-regexp "cl_khr_[a-zA-Z][a-zA-Z_0-9]+")
 
-(defvar-local opencl-font-lock-keywords
+(define-local-var opencl-font-lock-keywords
   `((,opencl-functions-regexp . font-lock-builtin-face)
     (,opencl-types-regexp . font-lock-type-face)
     (,opencl-constant-regexp . font-lock-constant-face)
