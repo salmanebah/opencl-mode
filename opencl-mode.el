b6f4a08574129f97ba79681e5e9ba36eee5f375a
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2016-02-18 20:38:46 +0100

    Cleanup code and comments

--- opencl-mode.el
+++ opencl-mode.el
@@ -1,11 +1,13 @@
-;;; Commentary:
-;;opencl-mode.el -- Quick and dirty syntax coloring for opencl kernels
+;;; opencl-mode.el --- Syntax coloring for opencl kernels
 
-;; Copyright (c) 2014 Salmane Bah <salmane.bah@u-bordeaux.fr>
+;; Copyright (c) 2014 - Salmane Bah <salmane.bah@u-bordeaux.fr>
 ;;
 ;; Author: Salmane Bah <salmane.bah@u-bordeaux.fr>
+;; Keywords: c, opencl
+;; URL: https://github.com/salmanebah/opencl-mode
+;; Version: 1.0
 
-;; This file is not part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -20,69 +22,88 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;; Installation:
+;;; Commentary:
+
+;;; Installation:
+
 ;; 1- Put this file in your load path or optionally add this line to your Emacs init file:
 ;;    (add-to-list 'load-path "/path/to/directory/where/opencl-mode.el/resides")
 ;; 2- Add these lines to your Emacs init file
 ;;    (require 'opencl-mode)
 ;;    (add-to-list 'auto-mode-alist '("\\.cl\\'" . opencl-mode))
-;; 3- Enjoy :)
 
 ;;; Code:
 
+;; for c-font-lock-keywords
+(require 'cc-fonts)
 
-(defvar-local *opencl-extension-color* "#A82848"
-  "opencl extension color")
+(make-variable-buffer-local
+ (defvar *opencl-extension-color* "#A82848"
+  "opencl extension color"))
 
 (defface font-lock-opencl-face
   `((t (:foreground ,*opencl-extension-color* :weight bold)))
   "custom face for cl-extension"
   :group 'opencl-faces)
 
-(defmacro define-local-var (variable value &optional docstring)
-  "Define `VARIABLE' with `VALUE' as local to this buffer with `DOCSTRING'."
-  `(if (= emacs-major-version 24)
-       (defvar-local ,variable ,value ,docstring)
-     (make-local-variable (defvar ,variable ,value ,docstring))))
+(make-variable-buffer-local
+ (defvar opencl-keywords-regexp
+   "\\(__\\)?\\(kernel\\|global\\|local\\|constant\\|private\\|read_only\\|write_only\\|read_write\\|enable\\|disable\\)[[:blank:]\n]+"
+   "Regexp for opencl keywords"))
 
-(define-local-var opencl-keywords-regexp
-  "\\(__\\)?\\(kernel\\|global\\|local\\|constant\\|private\\|read_only\\|write_only\\|read_write\\|enable\\|disable\\)[[:blank:]\n]+")
+(make-variable-buffer-local
+ (defvar opencl-functions-regexp
+   (regexp-opt '("get_work_dim" "get_global_size"
+		 "get_local_size" "get_global_id"
+		 "get_local_id" "get_num_groups"
+		 "get_group_id" "get_global_offset") 'words)
+   "Regexp for builtin opencl fucntions"))
 
-(define-local-var opencl-functions-regexp (regexp-opt '("get_work_dim" "get_global_size"
- 						    "get_local_size" "get_global_id"
-						    "get_local_id" "get_num_groups"
-						    "get_group_id" "get_global_offset") 'words))
+(make-variable-buffer-local
+  (defvar opencl-constant-regexp
+    (regexp-opt '("MAXFLOAT" "HUGE_VALF"
+		  "INFINITY" "NAN" "HUGE_VAL") 'words)
+    "Regexp for opencl constant"))
 
-(define-local-var opencl-constant-regexp (regexp-opt '("MAXFLOAT" "HUGE_VALF"
-						   "INFINITY" "NAN" "HUGE_VAL") 'words))
-(define-local-var opencl-types-regexp
-  "\\(char\\|short\\|half\\|int\\|double\\|float\\|long\\|uchar\\|ushort\\|uint\\|ulong\\)[[:digit:]]\\{0,2\\}[[:blank:]\n]+")
-(define-local-var opencl-scalar-types-regexp (regexp-opt '("bool" "size_t" "ptrdiff_t" "intptr_t" "uintptr_t")
-							 'words))
+(make-variable-buffer-local
+ (defvar opencl-types-regexp
+   "\\(char\\|short\\|half\\|int\\|double\\|float\\|long\\|uchar\\|ushort\\|uint\\|ulong\\)[[:digit:]]\\{0,2\\}[[:blank:]\n]+"
+   "Regexp for opencl primitive types"))
 
-(define-local-var opencl-image-type-regexp (regexp-opt '("image2d_t" "image3d_t"
-						     "image2d_array_t" "image3d_array_t"
-						     "image1d_array_t" "image1d_t"
-						     "image1d_buffer_t" "sampler_t"
-						     "event_t") 'words))
+(make-variable-buffer-local
+ (defvar opencl-scalar-types-regexp
+   (regexp-opt '("bool" "size_t" "ptrdiff_t" "intptr_t" "uintptr_t")
+	       'words)
+   "Regexp for opencl scalar types"))
 
-(define-local-var opencl-extension-regexp "cl_khr_[a-zA-Z][a-zA-Z_0-9]+")
+(make-variable-buffer-local
+ (defvar opencl-image-type-regexp
+   (regexp-opt '("image2d_t" "image3d_t"
+		 "image2d_array_t" "image3d_array_t"
+		 "image1d_array_t" "image1d_t"
+		 "image1d_buffer_t" "sampler_t"
+		 "event_t") 'words)
+   "Regexp for opencl image types"))
 
-(define-local-var opencl-font-lock-keywords
-  `((,opencl-functions-regexp . font-lock-builtin-face)
-    (,opencl-types-regexp . font-lock-type-face)
-    (,opencl-scalar-types-regexp . font-lock-type-face)
-    (,opencl-constant-regexp . font-lock-constant-face)
-    (,opencl-keywords-regexp . font-lock-keyword-face)
-    (,opencl-extension-regexp . 'font-lock-opencl-face)))
+(make-variable-buffer-local
+ (defvar opencl-extension-regexp "cl_khr_[a-zA-Z][a-zA-Z_0-9]+"
+   "Regex for opencl extensions"))
+
+(make-variable-buffer-local
+ (defvar opencl-font-lock-keywords
+   `((,opencl-functions-regexp . font-lock-builtin-face)
+     (,opencl-types-regexp . font-lock-type-face)
+     (,opencl-scalar-types-regexp . font-lock-type-face)
+     (,opencl-constant-regexp . font-lock-constant-face)
+     (,opencl-keywords-regexp . font-lock-keyword-face)
+     (,opencl-image-type-regexp . font-lock-type-face)
+     (,opencl-extension-regexp . 'font-lock-opencl-face))
+   "Font-lock for opencl keywords"))
+
 
 (define-derived-mode opencl-mode c-mode "Opencl"
   "Major mode for opencl kernel editing"
   (setcar font-lock-defaults (append c-font-lock-keywords opencl-font-lock-keywords)))
-
-;; add more keywords
-(font-lock-add-keywords 'opencl-mode
-			`((,opencl-image-type-regexp . font-lock-type-face)))
 
 (defun opencl-lookup ()
   "Get opencl documentation for string in region or point."
@@ -101,5 +122,4 @@
 (define-key opencl-mode-map (kbd "C-c ! d") 'opencl-lookup)
 
 (provide 'opencl-mode)
-
 ;;; opencl-mode.el ends here
