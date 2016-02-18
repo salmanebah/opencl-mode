a4a7390fb7d7a4adea520d2987d63d34f566373e
Salmane Bah <salmane.bah@ensimag.grenoble-inp.fr>
2016-02-18 21:23:02 +0100

    Update installation comments

--- opencl-mode.el
+++ opencl-mode.el
@@ -26,7 +26,7 @@
 
 ;;; Installation:
 
-;; 1- Put this file in your load path or optionally add this line to your Emacs init file:
+;; 1- Put opencl-mode.el in your load path or optionally add this line to your Emacs init file:
 ;;    (add-to-list 'load-path "/path/to/directory/where/opencl-mode.el/resides")
 ;; 2- Add these lines to your Emacs init file
 ;;    (require 'opencl-mode)
